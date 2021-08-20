28ac32cb4b4d722254f21db97cd82654e0858fd0
Wesley L. J. Manie <45335409+Wljmanie@users.noreply.github.com>
2021-08-20 10:57:16 +0200

    Initial commit

---
+++ CodingChallengeTemplate/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using CodingChallengeTemplate.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodingChallengeTemplate.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult AppJS()
+        {
+            return View();
+        }
+
+        public IActionResult CodeJS()
+        {
+            return View();
+        }
+
+        public IActionResult AppCS()
+        {
+            return View();
+        }
+
+        public IActionResult CodeCS()
+        {
+            return View();
+        }
+
+        //The method to set a cookie and the default culture and refreshes the page.
+        public IActionResult CultureManagement(string culture, string returnUrl)
+        {
+            Response.Cookies.Append(CookieRequestCultureProvider.DefaultCookieName, CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture)),
+                new CookieOptions { Expires = DateTimeOffset.Now.AddDays(30) });
+
+            return LocalRedirect(returnUrl);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
